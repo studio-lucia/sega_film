1683bf5d2e211a6fbc9be34d07b3e20bb6e8f459
Misty De Meo <mistydemeo@gmail.com>
2017-09-14 20:06:36 -0700

    Add root module documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
+//! This crate contains tools to parse, manipulate, and write Sega FILM containers.
+//! Sega FILM is a video container format which was used by Sega in Saturn games
+//! released between 1994 and 2000; it's the most common of the several video
+//! formats used on the Saturn.
+
+/// Contains tools for parsing the FILM container.
 pub mod container;
 mod utils;
