cf92d400d7d65301ee6fd7d1f2743980a2a5136a
Misty De Meo <mistydemeo@gmail.com>
2017-11-26 22:42:27 +1100

    Skip doc tests

--- ci/script.sh
+++ ci/script.sh
@@ -11,8 +11,8 @@ main() {
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cross test --target $TARGET --lib
+    cross test --target $TARGET --lib --release
 }
 
 # we don't run the "test phase" when doing deploys
